81912a0b408965e6f3b415621affe71c3dc90317
John Devost <John@Devost.net>
2024-03-24 20:05:29 -0400

    Updated to reflect negative vals and print if failed

--- analyzer.py
+++ analyzer.py
@@ -30,35 +30,58 @@ class Analyzer:
         if headless:
             chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        chrome_options.add_argument("--log-level=3")
 
         driver = webdriver.Chrome(options=chrome_options)
         index = 0
         while os.path.isfile("./results" + str(index) + ".csv"):
             index += 1
 
-        for stock in self.stonks.keys():
+        for stock in list(self.stonks.keys())[0:5]:
             try:
                 driver.get("https://www.cnn.com/markets/stocks/" + stock)
 
                 soup = bs(driver.page_source, "html.parser")
+
+                high = soup.find_all("g", class_="high-data")[0].find_all("tspan")[2]
+                median = soup.find_all("g", class_="median-data")[0].find_all("tspan")[
+                    2
+                ]
+                low = soup.find_all("g", class_="low-data")[0].find_all("tspan")[2]
+
+                high_txt = high.text
+                median_txt = median.text
+                low_txt = low.text
+
+                if high.attrs["fill"] == "#D50000":
+                    high_txt = "-" + high_txt
+                if median.attrs["fill"] == "#D50000":
+                    median_txt = "-" + median_txt
+                if low.attrs["fill"] == "#D50000":
+                    low_txt = "-" + low_txt
+
                 with open("./results" + str(index) + ".csv", "a") as file:
                     file.write(
                         stock
                         + ", "
-                        + soup.find_all("g", class_="high-data")[0]
-                        .find_all("tspan")[2]
-                        .text
+                        + high_txt
                         + ", "
-                        + soup.find_all("g", class_="median-data")[0]
-                        .find_all("tspan")[2]
-                        .text
+                        + median_txt
                         + ", "
-                        + soup.find_all("g", class_="low-data")[0]
-                        .find_all("tspan")[2]
-                        .text
+                        + low_txt
                         + ",\n"
                     )
             except Exception:
-                pass
+                with open("./results" + str(index) + ".csv", "a") as file:
+                    file.write(
+                        stock
+                        + ", "
+                        + "Failed"
+                        + ", "
+                        + "Failed"
+                        + ", "
+                        + "Failed"
+                        + ",\n"
+                    )
 
         driver.close()
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from analyzer import Analyzer
+import time
 
+start = time.perf_counter()
 analyzer = Analyzer()
 analyzer.get_stonks("stock_list.csv")
 analyzer.update_profiles()
+end = time.perf_counter()
+
+print("=============================================")
+print("\tTime taken:", end - start)
+print("=============================================")
